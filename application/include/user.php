bd3bc9140ac972c3d8d31928c2ed029b1d810691
Amin MasterkinG <lichwow_masterking@yahoo.com>
2019-05-31 21:18:23 +0430

    fix codestyle of normal change password.

--- application/include/user.php
+++ application/include/user.php
@@ -226,45 +226,55 @@ class user
     public static function normal_changepass()
     {
         global $antiXss;
-        if ($_POST['submit'] == 'changepass' && !empty($_POST['password']) && !empty($_POST['old_password']) && !empty($_POST['repassword']) && !empty($_POST['email']) && !empty($_POST['captcha']) && !empty($_SESSION['captcha'])) {
-            if (strtolower($_SESSION['captcha']) == strtolower($_POST['captcha'])) {
-                unset($_SESSION['captcha']);
-                if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-                    if ($_POST['password'] == $_POST['repassword']) {
-                        if (strlen($_POST['password']) >= 4 && strlen($_POST['password']) <= 16) {
-                            $userinfo = self::get_user_by_email(strtoupper($_POST['email']));
-                            if (!empty($userinfo['username'])) {
-                                $Old_hashed_pass = strtoupper(sha1(strtoupper($userinfo['username'] . ':' . $_POST['old_password'])));
-                                $hashed_pass = strtoupper(sha1(strtoupper($userinfo['username'] . ':' . $_POST['password'])));
-                                if (strtoupper($userinfo['sha_pass_hash']) == $Old_hashed_pass) {
-                                    database::$auth->update('account', [
-                                        'sha_pass_hash' => $antiXss->xss_clean($hashed_pass),
-                                        'sessionkey' => '',
-                                        'v' => '',
-                                        's' => ''
-                                    ], [
-                                        'id[=]' => $userinfo['id']
-                                    ]);
-                                    success_msg('Password has been changed.');
-                                } else {
-                                    error_msg('Old password is not valid.');
-                                }
-                            } else {
-                                error_msg('Email is not valid.');
-                            }
-                        } else {
-                            error_msg('Password length is not valid.');
-                        }
-                    } else {
-                        error_msg('Passwords is not equal.');
-                    }
-                } else {
-                    error_msg('Use valid email.');
-                }
-            } else {
-                error_msg('Captcha is not valid.');
-            }
+        if (!($_POST['submit'] == 'changepass' && !empty($_POST['password']) && !empty($_POST['old_password']) && !empty($_POST['repassword']) && !empty($_POST['email']) && !empty($_POST['captcha']) && !empty($_SESSION['captcha']))) {
+            return false;
         }
+
+        if (strtolower($_SESSION['captcha']) != strtolower($_POST['captcha'])) {
+            error_msg('Captcha is not valid.');
+            return false;
+        }
+
+        unset($_SESSION['captcha']);
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            error_msg('Use valid email.');
+            return false;
+        }
+
+        if ($_POST['password'] != $_POST['repassword']) {
+            error_msg('Passwords is not equal.');
+            return false;
+        }
+
+        if (!(strlen($_POST['password']) >= 4 && strlen($_POST['password']) <= 16)) {
+            error_msg('Password length is not valid.');
+            return false;
+        }
+
+        $userinfo = self::get_user_by_email(strtoupper($_POST['email']));
+        if (empty($userinfo['username'])) {
+            error_msg('Email is not valid.');
+            return false;
+        }
+
+        $Old_hashed_pass = strtoupper(sha1(strtoupper($userinfo['username'] . ':' . $_POST['old_password'])));
+        $hashed_pass = strtoupper(sha1(strtoupper($userinfo['username'] . ':' . $_POST['password'])));
+        if (strtoupper($userinfo['sha_pass_hash']) != $Old_hashed_pass) {
+            error_msg('Old password is not valid.');
+            return false;
+        }
+        
+        database::$auth->update('account', [
+            'sha_pass_hash' => $antiXss->xss_clean($hashed_pass),
+            'sessionkey' => '',
+            'v' => '',
+            's' => ''
+        ], [
+            'id[=]' => $userinfo['id']
+        ]);
+
+        success_msg('Password has been changed.');
+        return true;
     }
 
     public static function check_email_exists($email)
