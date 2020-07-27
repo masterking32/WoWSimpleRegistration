4ccc0183fc0509f4abb04c450168c0a3d31a0275
Amin.MasterkinG <Lichwow_masterking@yahoo.com>
2020-07-27 17:49:08 +0430

    Don't show deleted characters or characters without name for status. (Light Template)

--- template/light/tpl/main.php
+++ template/light/tpl/main.php
@@ -277,6 +277,9 @@ require_once 'header.php'; ?>
                             } else {
                                 echo '<table class="table table-striped"><thead><tr><th scope="col">Name</th><th scope="col">Race</th> <th scope="col">Class</th><th scope="col">Level</th></tr></thead><tbody>';
                                 foreach ($online_chars as $one_char) {
+                                    if(empty($one_char['name'])) {
+                                        continue;
+                                    }
                                     echo '<tr><th scope="row">' . $antiXss->xss_clean($one_char['name']) . '</th><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/race/' . $antiXss->xss_clean($one_char["race"]) . '-' . $antiXss->xss_clean($one_char["gender"]) . '.gif\'></td><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/class/' . $antiXss->xss_clean($one_char["class"]) . '.gif\'></td><td>' . $antiXss->xss_clean($one_char['level']) . '</td></tr>';
                                 }
                                 echo '</table>';
@@ -303,6 +306,9 @@ require_once 'header.php'; ?>
                                 echo '<table class="table table-striped"><thead><tr><th scope="col">Rank</th><th scope="col">Name</th><th scope="col">Race</th> <th scope="col">Class</th><th scope="col">Level</th><th scope="col">Play Time</th></tr></thead><tbody>';
                                 $m = 1;
                                 foreach ($data2show as $one_char) {
+                                    if(empty($one_char['name'])) {
+                                        continue;
+                                    }
                                     echo '<tr><td>' . $m++ . '<th scope="row">' . $antiXss->xss_clean($one_char['name']) . '</th><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/race/' . $antiXss->xss_clean($one_char["race"]) . '-' . $antiXss->xss_clean($one_char["gender"]) . '.gif\'></td><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/class/' . $antiXss->xss_clean($one_char["class"]) . '.gif\'></td><td>' . $antiXss->xss_clean($one_char['level']) . '</td><td>' . $antiXss->xss_clean(get_human_time_from_sec($one_char['totaltime'])) . '</td></tr>';
                                 }
                                 echo '</table>';
@@ -339,6 +345,9 @@ require_once 'header.php'; ?>
                                 echo '<table class="table table-striped"><thead><tr><th scope="col">Rank</th><th scope="col">Name</th><th scope="col">Race</th> <th scope="col">Class</th><th scope="col">Level</th><th scope="col">Kills</th></tr></thead><tbody>';
                                 $m = 1;
                                 foreach ($data2show as $one_char) {
+                                    if(empty($one_char['name'])) {
+                                        continue;
+                                    }
                                     echo '<tr><td>' . $m++ . '<th scope="row">' . $antiXss->xss_clean($one_char['name']) . '</th><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/race/' . $antiXss->xss_clean($one_char["race"]) . '-' . $antiXss->xss_clean($one_char["gender"]) . '.gif\'></td><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/class/' . $antiXss->xss_clean($one_char["class"]) . '.gif\'></td><td>' . $antiXss->xss_clean($one_char['level']) . '</td><td>' . $antiXss->xss_clean($one_char['totalKills']) . '</td></tr>';
                                 }
                                 echo '</table>';
@@ -361,6 +370,9 @@ require_once 'header.php'; ?>
                                 echo '<th scope="col">Honor Points</th></tr></thead><tbody>';
                                 $m = 1;
                                 foreach ($data2show as $one_char) {
+                                    if(empty($one_char['name'])) {
+                                        continue;
+                                    }
                                     echo '<tr><td>' . $m++ . '<th scope="row">' . $antiXss->xss_clean($one_char['name']) . '</th><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/race/' . $antiXss->xss_clean($one_char["race"]) . '-' . $antiXss->xss_clean($one_char["gender"]) . '.gif\'></td><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/class/' . $antiXss->xss_clean($one_char["class"]) . '.gif\'></td><td>' . $antiXss->xss_clean($one_char['level']) . '</td>';
 
                                     if (get_config('expansion') >= 6) {
@@ -386,6 +398,9 @@ require_once 'header.php'; ?>
                                 echo '<table class="table table-striped"><thead><tr><th scope="col">Rank</th><th scope="col">Name</th><th scope="col">Race</th> <th scope="col">Class</th><th scope="col">Level</th><th scope="col">Arena Points</th></tr></thead><tbody>';
                                 $m = 1;
                                 foreach ($data2show as $one_char) {
+                                    if(empty($one_char['name'])) {
+                                        continue;
+                                    }
                                     echo '<tr><td>' . $m++ . '<th scope="row">' . $antiXss->xss_clean($one_char['name']) . '</th><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/race/' . $antiXss->xss_clean($one_char["race"]) . '-' . $antiXss->xss_clean($one_char["gender"]) . '.gif\'></td><td><img src=\'' . get_config("baseurl") . '/template/' . $antiXss->xss_clean(get_config("template")) . '/images/class/' . $antiXss->xss_clean($one_char["class"]) . '.gif\'></td><td>' . $antiXss->xss_clean($one_char['level']) . '</td><td>' . $antiXss->xss_clean($one_char['arenaPoints']) . '</td></tr>';
                                 }
                                 echo '</table>';
@@ -403,6 +418,9 @@ require_once 'header.php'; ?>
                                 $m = 1;
                                 foreach ($data2show as $one_char) {
                                     $character_data = status::get_character_by_guid($onerealm['realmid'], $one_char['captainGuid']);
+                                    if(empty($character_data['name'])) {
+                                        continue;
+                                    }
                                     echo '<tr><td>' . $m++ . '<th scope="row">' . $antiXss->xss_clean($one_char['name']) . '</th><td>' . $antiXss->xss_clean($one_char['rating']) . '</td><td>' . (!empty($character_data["name"]) ? $antiXss->xss_clean($character_data['name']) : '-') . '</td></tr>';
                                 }
                                 echo '</table>';
