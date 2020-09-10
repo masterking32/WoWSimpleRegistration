fc1a91930935391149fe95622233f85ea3d83148
coolzoom <tommingc@gmail.com>
2020-09-10 22:13:00 +0800

    adding chinese-simplified and chinese-traditional
    update readme

---
+++ application/language/chinese-traditional.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @author Amin Mahmoudi (MasterkinG)
+ * @copyright    Copyright (c) 2019 - 2022, MsaterkinG32 Team, Inc. (https://masterking32.com)
+ * @link    https://masterking32.com
+ * @Description : It's not masterking32 framework !
+ **/
+
+$language['lang'] = 'chinese';
+$language['custom_css'] = '';
+$language['tpl_light_custom_css'] = '';
+$language['tpl_advance_custom_css'] = '';
+$language['tpl_icecrown_custom_css'] = '';
+$language['tpl_kaelthas_custom_css'] = '';
+
+$language['welcome_to'] = '歡迎來到';
+$language['welcome_message'] = '歡迎來到我們的伺服, 趕快加入我們吧! ';
+$language['get_started'] = '新手上路';
+$language['home'] = '主頁';
+$language['about'] = '關於';
+$language['how_to_connect'] = '如何連接';
+$language['register'] = '註冊';
+$language['server_status'] = '服務器狀態';
+$language['contact'] = '聯系';
+$language['server_information'] = '伺服訊息';
+$language['server_type'] = '伺服類型';
+$language['server_uptime'] = '伺服運行時間';
+$language['xp_rate'] = '經驗倍率';
+$language['drop_rate'] = '掉落倍率';
+$language['start_level'] = '開始等級';
+$language['max_level'] = '最高等級';
+$language['fixed_spells'] = '已修復技能';
+$language['fixed_dungeons'] = '已修復團本';
+$language['fixed_instances'] = '已修復小副本';
+$language['edit_on'] = '編輯於';
+$language['server_address'] = '伺服地址';
+$language['realmlist'] = '伺服列表';
+$language['create_account'] = '創建賬戶';
+$language['create_account_tip1'] = '首先，您必須創建壹個帳戶。該帳號用於登錄遊戲和我們的網站。點擊這裏打開註冊頁面';
+$language['download_game'] = '下載遊戲';
+$language['create_account_tip2'] = '安裝魔獸世界。妳可以從這裏(合法的)下載:Windows或Mac。確保升級到我們當前支持的遊戲版本，當前版本為'.get_config (game_version)'。鏡像源可以在這裏找到。';
+$language['setup_game'] = '安裝遊戲';
+$language['create_account_tip3'] = '打開"魔獸世界"目錄。默認目錄是"C:\Program Files\World of Warcraft"。進入目錄後，打開名為"data"的目錄，然後進入名為zhCN或enGB或enUS的目錄（取決於妳的客戶端語言）。';
+$language['change_server_address'] = '修改伺服地址';
+$language['create_account_tip4'] = '清空文本並更改為:';
+$language['server_rules'] = '伺服規則';
+$language['read_before_register'] = '註冊必讀';
+$language['read_our_rules'] = '請務必閱讀伺服規則';
+$language['rule'] = '規則';
+$language['create_new_game_account'] = '創建新的遊戲賬戶';
+$language['username'] = '用戶名';
+$language['password'] = '密碼';
+$language['retype_password'] = '確認密碼';
+$language['captcha'] = '驗證碼';
+$language['change_password'] = '更改密碼';
+$language['restore_password'] = '找回密碼';
+$language['vote_for_us'] = '給我們投票';
+$language['two_factor_authentication'] = '雙重認證';
+$language['two_factor_authentication_tip1'] = '安裝谷歌驗證（Google Authenticator）.';
+$language['two_factor_authentication_enable'] = '啟用雙重認證';
+$language['close'] = '關閉';
+$language['vote'] = '投票';
+$language['old_password'] = '舊密碼';
+$language['online_players'] = '在線玩家';
+$language['online_players_msg1'] = '最多顯示49個在線玩家 - 當前在線玩家:';
+$language['online_players_msg2'] = '當前無在線玩家';
+$language['name'] = '角色';
+$language['race'] = '種族';
+$language['class'] = '職業';
+$language['level'] = '級別';
+$language['top_players'] = '英雄榜';
+$language['play_time'] = '遊戲時間';
+$language['killers'] = '擊殺榜';
+$language['kills'] = '擊殺';
+$language['honor_points'] = '榮譽點數';
+$language['honor_level'] = '榮譽級別';
+$language['arena_points'] = '競技場點數';
+$language['arena_teams'] = '競技場團隊';
+$language['rank'] = '級別';
+$language['rating'] = '評級';
+$language['captain_name'] = '隊長名字';
+$language['frequently_questions'] = '常見問題';
+$language['question'] = '問題';
+$language['answer'] = '答案';
+$language['location'] = '位置';
+$language['call'] = '聯系電話';
+$language['restore_account_password'] = '找回賬戶密碼';
+$language['about_server'] = '關於伺服';
+$language['game_version'] = '遊戲版本';
+$language['server_patch'] = '遊戲補丁';
+$language['download'] = '下載';
+
+$language['use_valid_email'] = '請使用有效字母作為郵件地址。';
+$language['use_valid_username'] = '請使用有效字母作為用戶名。';
+$language['old_password_not_valid'] = '舊密碼不存在。';
+$language['passwords_not_equal'] = '密碼不符合。';
+$language['passwords_length'] = '密碼長度不對。';
+$language['username_length'] = '用戶名長度不對。';
+$language['username_or_email_exists'] = '該用戶名或郵件地址已經存在。';
+$language['email_exists'] = '該郵件地址已經存在。';
+$language['username_exists'] = '該用戶名已經存在。';
+$language['account_created'] = '賬戶被成功創建。';
+$language['error_try_again'] = '錯誤！請重試！';
+$language['password_changed'] = '密碼已被修改。';
+$language['email_not_correct'] = '電子郵件地址錯誤。';
+$language['username_not_correct'] = '用戶名錯誤。';
+$language['check_your_email'] = '請至註冊郵箱查收郵件，註意有可能在垃圾郵件收件箱';
+$language['account_is_not_valid'] = '賬戶不存在。';
+$language['vote_site_not_valid'] = '投票站點不存在。';
+$language['you_already_voted'] = '妳已經在該站點投過票了。';
+$language['captcha_not_valid'] = '驗證碼不存在';
+$language['hcaptcha_not_valid'] = '驗證碼不存在。';
+$language['recaptcha_not_valid'] = '驗證碼不存在。';
+$language['captcha_required'] = '需求驗證碼。';
