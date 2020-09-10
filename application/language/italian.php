710b80237e308e087d91c364bdf5c31a3b8b9707
Stefano Borzì <stefanoborzi32@gmail.com>
2020-09-10 13:16:48 +0200

    Create italian.php

---
+++ application/language/italian.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @author Amin Mahmoudi (MasterkinG)
+ * @copyright    Copyright (c) 2019 - 2022, MsaterkinG32 Team, Inc. (https://masterking32.com)
+ * @link    https://masterking32.com
+ * @Description : It's not masterking32 framework !
+ **/
+
+$language['lang'] = 'italian';
+$language['custom_css'] = '';
+$language['tpl_light_custom_css'] = '';
+$language['tpl_advance_custom_css'] = '';
+$language['tpl_icecrown_custom_css'] = '';
+$language['tpl_kaelthas_custom_css'] = '';
+
+$language['welcome_to'] = 'Benvenuto in';
+$language['welcome_message'] = 'Benvenuto nel nostro server privato, Partecipa adesso!';
+$language['get_started'] = 'Iniziare';
+$language['home'] = 'Home';
+$language['about'] = 'Chi Siamo';
+$language['how_to_connect'] = 'Come connettersi';
+$language['register'] = 'Registrati';
+$language['server_status'] = 'Server Status';
+$language['contact'] = 'Contatti';
+$language['server_information'] = 'Informazioni Server';
+$language['server_type'] = 'Tipo di Server';
+$language['server_uptime'] = 'Server Uptime';
+$language['xp_rate'] = 'XP Rate';
+$language['drop_rate'] = 'Drop Rate';
+$language['start_level'] = 'Start Level';
+$language['max_level'] = 'Max Level';
+$language['fixed_spells'] = 'Fixed Spells';
+$language['fixed_dungeons'] = 'Fixed Dungeons';
+$language['fixed_instances'] = 'Fixed Instances';
+$language['edit_on'] = 'Modifica';
+$language['server_address'] = 'Indirizzo del server';
+$language['realmlist'] = 'Realmlist';
+$language['create_account'] = 'Crea Account';
+$language['create_account_tip1'] = 'Prima di tutto, devi creare un account. L\' account è usato per entrare nel sito web ed in gioco. Clicca qui per aprire la pagina di registrazione';
+$language['download_game'] = 'Scarica il gioco';
+$language['create_account_tip2'] = 'Installa World of Warcraft. Puoi scaricarlo (legalmente) da qui: Windows o Mac. Assicurati di essere alla versione di gioco '. get_config('game_version').'. Eventuali patch e download link sono reperibili di seguito.';
+$language['setup_game'] = 'Configura il gioco';
+$language['create_account_tip3'] = 'Apri la cartella "World of Warcraft". Il percorso dei default dela cartella è "C:\Program Files\World of Warcraft". Una volta trovata, aprila ed apri la cartella "Data", quindi vai nella cartella "enUS" o "enGB".';
+$language['change_server_address'] = 'Cambia l\'indirizzo del server';
+$language['create_account_tip4'] = 'Cancella tutto il contenuto e cambialo in:';
+$language['server_rules'] = 'Regole del server';
+$language['read_before_register'] = 'LEGGI PRIMA DI REGISTRARTI';
+$language['read_our_rules'] = 'Perfavore, leggi il regolamento.';
+$language['rule'] = 'Regolamento';
+$language['create_new_game_account'] = 'Crea un account di gioco.';
+$language['email'] = 'Email';
+$language['username'] = 'Nome utente';
+$language['password'] = 'Password';
+$language['retype_password'] = 'Re-Password';
+$language['captcha'] = 'Captcha';
+$language['change_password'] = 'Cambia Password';
+$language['restore_password'] = 'Recupera Password';
+$language['vote_for_us'] = 'Vota';
+$language['two_factor_authentication'] = 'Two-Factor Authentication';
+$language['two_factor_authentication_tip1'] = 'Installa Google Authenticator.';
+$language['two_factor_authentication_enable'] = 'Abilita 2FA';
+$language['close'] = 'Chiudi';
+$language['vote'] = 'Vota';
+$language['old_password'] = 'Vecchia Password';
+$language['online_players'] = 'Giocatori Online';
+$language['online_players_msg1'] = 'Limitato a mostrare 49 giocatori - Giocatori online:';
+$language['online_players_msg2'] = 'Nessun giocatore online';
+$language['name'] = 'Name';
+$language['race'] = 'Race';
+$language['class'] = 'Class';
+$language['level'] = 'Level';
+$language['top_players'] = 'Top Players';
+$language['play_time'] = 'Play Time';
+$language['killers'] = 'Killers';
+$language['kills'] = 'Kills';
+$language['honor_points'] = 'Honor Points';
+$language['honor_level'] = 'Honor level';
+$language['arena_points'] = 'Arena Points';
+$language['arena_teams'] = 'Arena Teams';
+$language['rank'] = 'Rank';
+$language['rating'] = 'Rating';
+$language['captain_name'] = 'Captain Name';
+$language['frequently_questions'] = 'Domande frequenti (FAQ)';
+$language['question'] = 'Domanda';
+$language['answer'] = 'Risposta';
+$language['location'] = 'Locazione';
+$language['call'] = 'Call';
+$language['restore_account_password'] = 'Ripristina password dell\'account';
+$language['about_server'] = 'Informazioni server';
+$language['game_version'] = 'Versione del gioco';
+
+$language['use_valid_email'] = 'Usa caratteri validi per l\'email.';
+$language['use_valid_username'] = 'Usa caratteri validi per il nome utente.';
+$language['old_password_not_valid'] = 'La vecchia password non è valida.';
+$language['passwords_not_equal'] = 'Le password non sono uguali.';
+$language['passwords_length'] = 'La lunghezza della password non è valida.';
+$language['username_length'] = 'La lunghezza del nome utente non è valida.';
+$language['username_or_email_exists'] = 'Il nome utente o l\'email già esiste.';
+$language['email_exists'] = 'L\'email esiste.';
+$language['username_exists'] = 'Il nome utente esiste.';
+$language['account_created'] = 'Il tuo account è stato creato.';
+$language['error_try_again'] = 'ERRORE!, Riprova!';
+$language['password_changed'] = 'La password è stata cambiata.';
+$language['email_not_correct'] = 'L\' email non è corretta.';
+$language['username_not_correct'] = 'L\'username non è corretto.';
+$language['check_your_email'] = 'Controlla la tua email, (Controlla anche la posta in SPAM).';
+$language['account_is_not_valid'] = 'L\'account non è valido.';
+$language['vote_site_not_valid'] = 'Il voto non è valido!';
+$language['you_already_voted'] = 'Hai già votato questo sito.';
+$language['captcha_not_valid'] = 'Il Captcha non è valido.';
+$language['hcaptcha_not_valid'] = 'L\' HCaptcha non è valido.';
+$language['recaptcha_not_valid'] = 'Il ReCaptcha non è valido.';
+$language['captcha_required'] = 'Compila il Captcha.';
