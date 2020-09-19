2822dcb855e78e40ccccad626bf758ad8e39cadf
masterking32 <lichwow_masterking@yahoo.com>
2020-09-19 14:51:40 +0430

    Update comments.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author Amin Mahmoudi (MasterkinG)
- * @copyright    Copyright (c) 2019 - 2022, MsaterkinG32 Team, Inc. (https://masterking32.com)
+ * @copyright    Copyright (c) 2019 - 2022, MasterkinG32 Team, Inc. (https://masterking32.com)
  * @link    https://masterking32.com
  * @Description : It's not masterking32 framework !
  **/
