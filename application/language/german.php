f1dbd05faee1fc3ab41e2c1581b91e4e77b82a39
DuelistRag3 <christian.dullin@web.de>
2020-09-26 15:37:53 +0200

    Added german language file. * no claim for correctness

---
+++ application/language/german.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @author Amin Mahmoudi (MasterkinG)
+ * @copyright    Copyright (c) 2019 - 2020, MasterkinG32. (https://masterking32.com)
+ * @link    https://masterking32.com
+ * @Description : It's not masterking32 framework !
+ **/
+
+$language['lang'] = 'german';
+$language['custom_css'] = '';
+$language['tpl_light_custom_css'] = '';
+$language['tpl_advance_custom_css'] = '';
+$language['tpl_icecrown_custom_css'] = '';
+$language['tpl_kaelthas_custom_css'] = '';
+$language['tpl_battleforazeroth_custom_css'] = '';
+
+$language['welcome_to'] = 'Willkommen zu';
+$language['welcome_message'] = 'Willkommen bei unserem Privat Server, Tritt uns bei!';
+$language['get_started'] = 'Starte jetzt';
+$language['home'] = 'Start';
+$language['about'] = 'Über uns';
+$language['how_to_connect'] = 'Wie du dich verbinden kannst';
+$language['register'] = 'Registrieren';
+$language['server_status'] = 'Server-Status';
+$language['contact'] = 'Kontakt';
+$language['server_information'] = 'Server Information';
+$language['server_type'] = 'Server-Typ';
+$language['server_uptime'] = 'Server Uptime';
+$language['xp_rate'] = 'Erfahrungs Rate';
+$language['drop_rate'] = 'Drop Rate';
+$language['start_level'] = 'Start Level';
+$language['max_level'] = 'Max Level';
+$language['fixed_spells'] = 'Behobene Zauber';
+$language['fixed_dungeons'] = 'Behobene Dungeons';
+$language['fixed_instances'] = 'Behobene Instanzen';
+$language['edit_on'] = 'Hier bearbeiten:';
+$language['server_address'] = 'Server-Adresse';
+$language['realmlist'] = 'Realmliste';
+$language['create_account'] = 'Account erstellen';
+$language['create_account_tip1'] = 'Als erstes, musst du dir einen Account erstellen. Diesen Account nutzt du sowohl zum Login auf der Website als auch im spiel. Klicke hier um das Formular zu öffnen';
+$language['download_game'] = 'Spiel Runterladen';
+$language['create_account_tip2'] = 'Lade World of Warcraft runter. Du kannst es (Legal) hier runterladen: Windows oder Mac. Gehe sicher das es die Richtige version ist:'. get_config('game_version').'.';
+$language['setup_game'] = 'Spiel richtig einstellen';
+$language['create_account_tip3'] = 'Öffne deinen "World of Warcraft" Ordner. Normalerweise ist das "C:\Program Files\World of Warcraft". Wenn du ihn gefunden hast, öffne den Ordner "data", gehe dann in den Ordner enUS, enGB oder deDE, jenachdem welche Sprache dein World of Warcraft hat.';
+$language['change_server_address'] = 'Ändere die Server-Adresse';
+$language['create_account_tip4'] = 'Markiere den ganzen Text und ersetze ihn mit:';
+$language['server_rules'] = 'Server Regeln';
+$language['read_before_register'] = 'VOR DEM ANMELDEN LESEN!';
+$language['read_our_rules'] = 'Bitte lese unsere Regeln.';
+$language['rule'] = 'Regel';
+$language['create_new_game_account'] = 'Neuen Account erstellen.';
+$language['email'] = 'Email';
+$language['username'] = 'Nutzername';
+$language['password'] = 'Passwort';
+$language['retype_password'] = 'Passwort erneut eingeben';
+$language['captcha'] = 'Captcha';
+$language['change_password'] = 'Passwort ändern';
+$language['restore_password'] = 'Passwort vergessen';
+$language['vote_for_us'] = 'Stimme für uns ab';
+$language['two_factor_authentication'] = 'Zwei-Faktor-Authentifizierung';
+$language['two_factor_authentication_tip1'] = 'Installiere die Google Authenticator App.';
+$language['two_factor_authentication_enable'] = 'Aktiviere 2FA';
+$language['close'] = 'Schließen';
+$language['vote'] = 'Abstimmen';
+$language['old_password'] = 'Altes Passwort';
+$language['online_players'] = 'Online Spieler';
+$language['online_players_msg1'] = 'Limitiert auf 49 Spieler - Online Spieler:';
+$language['online_players_msg2'] = 'Aktuell sind keine Spieler Online';
+$language['name'] = 'Name';
+$language['race'] = 'Rasse';
+$language['class'] = 'Klasse';
+$language['level'] = 'Level';
+$language['top_players'] = 'Top Spieler';
+$language['play_time'] = 'Spielzeit';
+$language['killers'] = 'Meiste PVP kills';
+$language['kills'] = 'Kills';
+$language['honor_points'] = 'Ehren Punkte';
+$language['honor_level'] = 'Ehren level';
+$language['arena_points'] = 'Arena Punkte';
+$language['arena_teams'] = 'Arena Teams';
+$language['rank'] = 'Rang';
+$language['rating'] = 'Bewertung';
+$language['captain_name'] = 'Kapitäns Name';
+$language['frequently_questions'] = 'Häufig gestellte Fragen';
+$language['question'] = 'Frage';
+$language['answer'] = 'Antwort';
+$language['location'] = 'Standort';
+$language['call'] = 'Anrufen';
+$language['restore_account_password'] = 'Account Passwort wiederherstellen';
+$language['about_server'] = 'Über den Server';
+$language['game_version'] = 'Spiel version';
+$language['server_patch'] = 'Server Patch';
+$language['download'] = 'Download';
+
+$language['use_valid_email'] = 'Gültige Zeichen für E-Mail verwenden.';
+$language['use_valid_username'] = 'Gültige Zeichen für den Benutzernamen verwenden.';
+$language['old_password_not_valid'] = 'Altes Passwort ist nicht korrekt.';
+$language['passwords_not_equal'] = 'Passwörter stimmen nicht überein.';
+$language['passwords_length'] = 'Passwort zu lang oder zu kurz.';
+$language['username_length'] = 'Nutzername zu lang oder zu kurz.';
+$language['username_or_email_exists'] = 'Benutzername oder E-Mail existiert bereits.';
+$language['email_exists'] = 'E-Mail existiert bereits.';
+$language['username_exists'] = 'Benutzername existiert bereits.';
+$language['account_created'] = 'Dein account wurde erstellt.';
+$language['error_try_again'] = 'FEHLER!, Bitte versuch es erneut!';
+$language['password_changed'] = 'Passwort wurde geändert.';
+$language['email_not_correct'] = 'E-Mail falsch.';
+$language['username_not_correct'] = 'Benutzername falsch.';
+$language['check_your_email'] = 'Überprüfe deinen Posteingang. (Auch SPAM/Junk).';
+$language['account_is_not_valid'] = 'Benutzer ist nicht gültig.';
+$language['vote_site_not_valid'] = 'Vote seite ist nicht güötig!';
+$language['you_already_voted'] = 'Auf dieser seite hast du bereits abgestimmt.';
+$language['captcha_not_valid'] = 'Captcha ist nicht gültig.';
+$language['hcaptcha_not_valid'] = 'HCaptcha ist nicht gültig.';
+$language['recaptcha_not_valid'] = 'ReCaptcha ist nicht gültig.';
+$language['captcha_required'] = 'Captcha wird benötigt.';
